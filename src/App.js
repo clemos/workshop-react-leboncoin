9873d59e17541989236da4d524b127decbed13cc
clemos <cl3mos@gmail.com>
2018-10-23 13:12:49 +0200

    step 3

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function MonBouton(props) {
-  return <button>{props.title}</button>
+  return (
+    <button 
+      onClick={props.onClick}
+    >
+      {props.title}
+    </button>
+  );
 }
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <MonBouton title="Cliquez ici" />
+        <MonBouton 
+          title="Cliquez ici"
+          onClick={() => window.alert('bonjour')}
+        />
+        <MonBouton 
+          title="Ne cliquez pas ici"
+          onClick={() => window.alert('au revoir')}
+        />
       </div>
     );
   }
