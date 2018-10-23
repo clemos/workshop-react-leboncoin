89d5ebe15122eb7da2a3ae25416c4cb7fa08a44d
clemos <cl3mos@gmail.com>
2018-10-23 13:32:57 +0200

    step 4: composants imbriqués

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function MonCadre(props) {
+  return (
+    <div style={{border: "1px solid red"}}>
+      <h1>{props.title}</h1>
+      {props.children}
+    </div>
+  )
+}
+
 function MonBouton(props) {
   return (
     <button 
@@ -17,6 +26,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <MonCadre title="Cadre 1 !">
+          <p>regardez mon site</p>
+          <a href="https://silexlabs.org">ici</a>
+        </MonCadre>
         <MonBouton 
           title="Cliquez ici"
           onClick={() => window.alert('bonjour')}
